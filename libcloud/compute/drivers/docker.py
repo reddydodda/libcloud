07be7ad5de7140f60b0d7bfa2e1915bb0ee95fee
Markos Gogoulos <mgogoulos@mist.io>
2015-01-28 15:49:05 +0200

    add some doc for docker tls

--- libcloud/compute/drivers/docker.py
+++ libcloud/compute/drivers/docker.py
@@ -101,6 +101,9 @@ class DockerNodeDriver(NodeDriver):
     or connecting to http basic auth protected https host:
     >>> conn = driver('user', 'pass', host='https://198.61.239.128', port=443)
 
+    connect with tls authentication, by providing a hostname, port, a private
+    key file (.pem) and certificate (.pem) file
+    >>> conn = driver(host='https://198.61.239.128', port=4243, key_file='key.pem', cert_file='cert.pem')
     """
 
     type = Provider.DOCKER
@@ -124,6 +127,9 @@ class DockerNodeDriver(NodeDriver):
                 host = host.strip(prefix)
 
         if key_file or cert_file:
+            # docker tls authentication - https://docs.docker.com/articles/https/
+            # We pass two files, a key_file with the private key and cert_file with the certificate
+            # libcloud will handle them through LibcloudHTTPSConnection
             if not (key_file and cert_file):
                     raise Exception('Needs both private key file and certificate file for tls authentication')
             self.connection.key_file = key_file
