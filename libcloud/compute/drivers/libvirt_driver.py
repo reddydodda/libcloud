1f9ac02055908f06199a32f2ee3ee4c37ab731e0
Markos Gogoulos <mgogoulos@mist.io>
2015-04-28 17:40:15 +0300

    fixes libvirt errors

--- libcloud/compute/drivers/libvirt_driver.py
+++ libcloud/compute/drivers/libvirt_driver.py
@@ -99,6 +99,7 @@ class LibvirtNodeDriver(NodeDriver):
             self.connection = libvirt.open(uri)
             signal.alarm(0)      # Disable the alarm
         except Exception as exc:
+            signal.alarm(0)      # Disable the alarm
             if 'Could not resolve' in exc.message:
                 raise Exception("Make sure hostname is accessible")
             if 'Connection refused' in exc.message:
@@ -107,7 +108,6 @@ class LibvirtNodeDriver(NodeDriver):
                 raise Exception("Make sure ssh key and username are valid")
             if 'End of file while reading data' in exc.message:
                 raise Exception("Make sure libvirt is running and user %s is authorised to connect" % user)
-            signal.alarm(0)      # Disable the alarm
             raise Exception("Connection error")
 
     def list_nodes(self, show_hypervisor=True):
